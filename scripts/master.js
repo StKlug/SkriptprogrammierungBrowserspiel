f6ee08ec3833d5c6d6acccf8529d0282c247ab8b
MarijaU <marija.urbanowitsch@rwth-aachen.de>
2015-07-22 17:22:46 +0200

    merge

--- scripts/master.js
+++ scripts/master.js
@@ -11,10 +11,12 @@ $(function()
 				return false;
 			}
 			switch(target){
+				case "home":
+					window.location.href= "home.php";
 				case "game":
 					window.location.href= "game.php";
 				case "profile":
-					window.location.href="profile.php";
+					window.location.href="Profil_bearbeiten.php";
 				case "login":
 					window.location.href="login.php";
 				default:
