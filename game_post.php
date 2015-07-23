1c3e23c684d958d5f57488180d4f6de4025fc42b
StefanKlug1 <stefan@123-edv.de>
2015-07-23 15:45:41 +0200

    Nachrichten vom GameServer ueberarbeitet

--- game_post.php
+++ game_post.php
@@ -5,59 +5,62 @@ session_start();
 require_once("lib/Board.php");
 require_once("lib/RandomAI.php");
 
-$action = isset($_POST['action']) ? $_POST['action'] : '';
-
 const HUMAN_PLAYER = 1;
 const AI = 2;
 
-$debugInfo = "";
+$action = isset($_POST['action']) ? $_POST['action'] : '';
 
-if($action === 'newGame'){
+$message = ""; // possible values: "WON", "LOST", "TIE", "ILLEGAL_MOVE"
+
+if ($action === 'newGame') {
     $_SESSION['expect_turn'] = 'false';
     $board = new Board();
     $_SESSION['board'] = serialize($board);
     $_SESSION['expect_turn'] = 'true';
-}
-else if($action === 'turn'){
+} else if ($action === 'turn') {
     $board = unserialize($_SESSION['board']);
-    if($_SESSION['expect_turn'] === 'true' && isset($_POST['column'])){
+    if ($_SESSION['expect_turn'] === 'true' && isset($_POST['column'])) {
         $_SESSION['expect_turn'] = 'false';
-        $columnRange = array ('options' => array ('min_range' => 0, 'max_range' => Board::COLUMN_COUNT - 1));
+        $columnRange = array('options' => array('min_range' => 0, 'max_range' => Board::COLUMN_COUNT - 1));
         $column = filter_input(INPUT_POST, 'column', FILTER_VALIDATE_INT, $columnRange); // $column ist nun null, false oder ein gueltiger Wert
-        if(isset($column) && $column !== false){
-            $debugInfo = $debugInfo . "empfangene column: " . $column . "; ";
+        if (isset($column) && $column !== false) {
+            $cancel = false;
             $successfulMove = $board->drop(HUMAN_PLAYER, $column);
-            if($board->hasWon(HUMAN_PLAYER)){
-                $debugInfo = $debugInfo . "Spieler hat gewonnen; ";
-                // TODO Gewonnen-Nachricht
+            if($successfulMove === false){
+                $message = "ILLEGAL_MOVE";
             }
-            if($board->isFull() === true){
-                // TODO Spiel-unentschieden-Nachricht
-                $debugInfo = $debugInfo . "Board ist voll; ";
+            if ($board->hasWon(HUMAN_PLAYER)) {
+                $cancel = true;
+                $message = "WON";
             }
-            if($successfulMove === true){
-                $debugInfo = $debugInfo . "erfolgreicher Zug des Spielers; ";
-                $successfulMove = RandomAI::move($board, AI);
-            }
-            if($successfulMove === true){
-                $debugInfo = $debugInfo . "erfolgreicher Zug der AI; ";
-                if($board->hasWon(AI)){
-                    // TODO Verloren-Nachricht
-                    $debugInfo = $debugInfo . "AI hat gewonnen; ";
+            if ($cancel !== true) {
+                if ($board->isFull() === true) {
+                    $cancel = true;
+                    $message = "TIE";
                 }
-                $_SESSION['board'] = serialize($board);
-                $_SESSION['expect_turn'] = 'true';
+                if ($cancel !== true) {
+                    if ($successfulMove === true) {
+                        $successfulMove = RandomAI::move($board, AI);
+                    }
+                    if ($successfulMove === true) {
+                        if ($board->hasWon(AI)) {
+                            $message = "LOST";
+                        }
+                    }
+                }
             }
+            $_SESSION['board'] = serialize($board);
+            $_SESSION['expect_turn'] = 'true';
         } else {
-            $debugInfo = $debugInfo . "illegale column empfangen: " . $column . "; ";
+            $message = "ILLEGAL_MOVE";
         }
     }
 }
 
-if(isset($_SESSION['board'])){
+if (isset($_SESSION['board'])) {
 
     $array = array();
     $array["board"] = unserialize($_SESSION['board'])->toJSON();
-    $array["action"] = $debugInfo;
+    $array["message"] = $message;
     echo json_encode($array);
 }
