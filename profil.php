878dff6e36b9fb09431060fc103f48de67031663
MarijaU <marija.urbanowitsch@rwth-aachen.de>
2015-07-23 15:05:54 +0200

    nachricht senden eingefuegt

--- profil.php
+++ profil.php
@@ -1,13 +1,20 @@
 <?php
+
 	require_once'lib/Database.class.php';
 	require_once 'lib/Template.class.php';
+	if(isset(($_GET['send'])))
+	{
+		header('Location: sendMessage.php ');
 
+	}else
+	{
 		 $tpl = new Template();
 
 		$dbh = Database::getInstance();
 
 		$query = $dbh->prepare("SELECT nick,points,beschreibung From user WHERE email = :mail");
-		$mail = htmlspecialchars($POST['email']);
+		$mail = htmlspecialchars($_POST['email']);
+		$_SESSION['sendEmail'] =$_POST['email'];
 		$query->bindParam(':mail', $mail);
 		$query->execute();
 		$row = $query->fetch();
@@ -20,4 +27,5 @@
 		 $tpl->assign('beschr', $beschr);
 
 		 $tpl->display('templates/profil.tpl');
+	}
 ?>
